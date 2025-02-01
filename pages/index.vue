f1d88ecb3c407a78150a6786e68c58292b1819ca
Mateusz Horczak <82621@student.pb.edu.pl>
2025-02-01 01:11:22 +0100

    feat(ability to exclude previous questions)

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
+const { data: questions, refresh } = await useFetch('/api/questions')
 
-  const { data: questions } = await useFetch('/api/questions')
-
+const duplicate = ref(false)
 
 const getNewQuestion = () => {
   if (!questions.value) {
@@ -9,10 +9,17 @@ const getNewQuestion = () => {
   }
   const i = Math.floor(Math.random() * questions.value.length)
   currentQuestion.value = questions.value[i]
+
+  if (!duplicate.value) {
+    questions.value = questions.value.filter(q => q !== currentQuestion.value)
+  }
+}
+
+const reset = () => {
+  refresh()
 }
 
 const currentQuestion = ref('')
-
 </script>
 
 <template>
@@ -23,13 +30,43 @@ const currentQuestion = ref('')
           <AtomsHeading>Random questions</AtomsHeading>
         </template>
 
-        <div class="h-32">
+        <div class="h-32 relative">
           <AtomsParagraph v-if="!currentQuestion">Get your first question!</AtomsParagraph>
           <AtomsParagraph v-else>{{ currentQuestion }}</AtomsParagraph>
+          <AtomsParagraph v-if="questions" class="absolute right-0 bottom-0">{{ questions.length }} questions left
+          </AtomsParagraph>
         </div>
 
         <template #footer>
-          <UButton label="Next question" variant="solid" type="button" size="lg" @click="getNewQuestion" icon="i-mdi-arrow-right" />
+          <div class="flex justify-between">
+            <UButton label="Next question"
+                     variant="solid"
+                     type="button"
+                     size="lg"
+                     @click="getNewQuestion"
+                     icon="i-mdi-arrow-right"
+            />
+
+            <div class="flex space-x-4">
+              <UButton label="Reset"
+                       variant="solid"
+                       type="button"
+                       size="lg"
+                       @click="reset"
+                       icon="i-mdi-reload"
+              />
+
+              <div class="flex space-x-1 items-center">
+                <UToggle v-model="duplicate"
+                         on-icon="i-mdi-check"
+                         off-icon="i-mdi-close"
+                         size="lg"
+                />
+                <AtomsParagraph>Duplicates</AtomsParagraph>
+              </div>
+            </div>
+          </div>
+
         </template>
       </UCard>
     </div>
